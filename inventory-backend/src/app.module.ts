3209728dafb872883a0df5e7c4d7c42099421821
Frosmin <simonabastomart@gmail.com>
2026-02-22 20:19:36 -0400

    feat: integrate TypeORM and PostgreSQL for product management
    - Added TypeORM and PostgreSQL dependencies to package.json.
    - Configured TypeORM in app.module.ts to connect to PostgreSQL using DATABASE_URL.
    - Created Product entity with fields: id, name, price, stock, createdAt, and updatedAt.
    - Updated ProductsModule to include TypeOrmModule for Product entity.
    - Enabled global configuration with ConfigModule for environment variables.

--- inventory-backend/src/app.module.ts
+++ inventory-backend/src/app.module.ts
@@ -2,10 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      entities: [],
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+
+
+    }),],
   controllers: [AppController],
   providers: [AppService],
+
+
 })
-export class AppModule {}
+export class AppModule { }
--- inventory-backend/src/products/entities/product.entity.ts
+++ inventory-backend/src/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  name!: string;
+
+  @Column({ type: 'numeric', precision: 10, scale: 2, default: 0 })
+  price!: number;
+
+  @Column({ type: 'int', default: 0 })
+  stock!: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+}
