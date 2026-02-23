23ac2c6921a9c9c1100b91976ecb3ecbe48304ca
Frosmin <simonabastomart@gmail.com>
2026-02-22 23:45:17 -0400

    feat: implement product creation with validation and Swagger documentation

--- inventory-backend/src/products/products.controller.ts
+++ inventory-backend/src/products/products.controller.ts
@@ -2,13 +2,19 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
+@Post()
+  @ApiOperation({ summary: 'Crear un nuevo producto' }) // Describe qué hace la ruta en Swagger
+  @ApiResponse({ status: 201, description: 'El producto fue creado exitosamente.' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos (Bad Request).' })
   create(@Body() createProductDto: CreateProductDto) {
+    // Si la petición llega hasta aquí, significa que pasó todas las validaciones de seguridad
     return this.productsService.create(createProductDto);
   }
 
--- inventory-backend/src/products/products.service.ts
+++ inventory-backend/src/products/products.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productRepository.create(createProductDto);
+    return await this.productRepository.save(product);
   }
 
   findAll() {
